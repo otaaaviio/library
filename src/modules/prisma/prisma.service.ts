0f0659e41a98e33072b1f152b039583773f02c87
Otavio G <oglamberty@inf.ufsm.br>
2024-05-25 15:26:50 -0300

    prisma mudanlças

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,54 +1,24 @@
 import {
-  INestApplication,
-  Injectable,
-  Logger,
-  OnModuleInit,
+    Injectable,
+    Logger,
+    OnModuleInit,
 } from '@nestjs/common';
-import { Prisma, PrismaClient } from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient<
-    Prisma.PrismaClientOptions,
-    'query' | 'info' | 'warn' | 'error' | 'beforeExit'
-  >
-  implements OnModuleInit
-{
-  constructor() {
-    super({
-      log: [
-        {
-          emit: 'event',
-          level: 'query',
-        },
-        {
-          emit: 'event',
-          level: 'error',
-        },
-        {
-          emit: 'stdout',
-          level: 'info',
-        },
-        {
-          emit: 'stdout',
-          level: 'warn',
-        },
-      ],
-    });
-  }
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger(PrismaService.name);
 
-  async onModuleInit() {
-    try {
-      await this.$connect();
-    } catch (err) {
-      Logger.error('Failed to connect to Prisma:', err);
-      throw new err();
+    constructor() {
+        super();
     }
-  }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
+    async onModuleInit() {
+        try {
+            await this.$connect();
+        } catch (err) {
+            this.logger.error('Failed to connect to Prisma:', err);
+            throw new err();
+        }
+    }
 }
